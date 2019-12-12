668a190fecc5ea9c395e291378b0266b4cd4d316
Devin Warrick <dev.warr@gmail.com>
2019-12-12 01:30:37 -0600

    Add match; button conditionally renders if there is a current match

--- src/views/CreateUser.js
+++ src/views/CreateUser.js
@@ -3,10 +3,13 @@ import { GameContext } from "../contexts/GameContext"
 
 export default function CreateUser() {
     const [username, setUsername] = React.useState()
-    const { socket } = React.useContext(GameContext)
+    const { socket, match } = React.useContext(GameContext)
+    console.log("match", match)
 
     const goToMatch = e => {
         e.preventDefault()
+        if (username.length < 2) return
+
         if (e.target.id === "create") {
             socket.emit("createMatch", username)
         }
@@ -16,12 +19,11 @@ export default function CreateUser() {
         <div className="create-user">
             <input
                 type="text"
-                name="username"
                 value={username}
                 onChange={e => setUsername(e.target.value)}
             />
             <button id="create" onClick={goToMatch}>
-                Create a new Match
+                {match ? `Join and Play` : `Create a new Match`}
             </button>
         </div>
     )
